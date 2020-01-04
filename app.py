80ff77fc63f4e641c793f1334650a07ee184982c
Brian Cohn <brian.cohn@usc.edu>
2020-01-03 17:18:55 -0800

    op auth, token, and listening

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+import pdb
+# app.py
+from flask import Flask, request, jsonify, abort, Response
+import time
+import uuid
+from os import listdir
+from os.path import isfile, join
+
+app = Flask(__name__)
+
+
+#helper functions
+################
+# gets list of strings, one for each filename in a target directory
+def get_files(mypath):
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return onlyfiles
+
+# trims filename extensions from a list of filenames, including the dot
+def basenames(file_list):
+    return [os.path.splitext(f)[0] for f in file_list]
+
+def get_last_observation(filepath):
+    with open(filepath, "rb") as f:
+        first = f.readline()        # Read the first line.
+        f.seek(-2, os.SEEK_END)     # Jump to the second last byte.
+        while f.read(1) != b"\n":   # Until EOL is found...
+            f.seek(-2, os.SEEK_CUR) # ...jump back the read byte plus one more.
+        last = f.readline()         # Read last line.
+    return last.decode("utf-8").rstrip().split(",")
+
+# returns the list of tokens in the userdata folder
+def active_tokens():
+    return basenames(get_files("userdata"))
+###############
+#API
+@app.route('/listen/', methods=['GET'])
+def listen():
+    token = request.args.get("token")
+    if token not in active_tokens():
+        abort(Response("Error: The token you tried (%s) is not active. Go make a new one."%token, status=401))
+
+    time, obs = get_last_observation("userdata/%s.txt"%token)
+    return jsonify({
+        "unixtime": float(time),
+        "value": float(obs)
+        })
+
+@app.route('/update/', methods=['POST'])
+def update():
+    token = request.args.get("token")
+    if token not in active_tokens():
+        abort(Response("Error: The token you tried (%s) is not active. Go make a new one."%token, status=401))
+
+    obs = request.args.get("obs")
+    with open("userdata/%s.txt"%token, "a") as myfile:
+        myfile.write("%s,%s\n"%(time.time(),obs))
+    return "posted"
+
+def gen_new_token():
+    new_token = uuid.uuid4()
+    user_file = open("userdata/%s.txt"%new_token,"a")
+    now = time.time()
+    L = ["APN:unknown\n", "init:%s\n"%now]
+    user_file.writelines(L) 
+    user_file.write("%s,%s\n"%(time.time(),-1))
+    user_file.close()
+    return new_token
+
+@app.route('/newtoken/', methods=['POST'])
+def newtoken():
+    return jsonify({
+        "token": f"%s"%gen_new_token(),
+        "METHOD" : "POST"
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Token: %s</h1><br><img src=\"https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=%s\"><br>"%(gen_new_token(),gen_new_token())
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
---
+++ simulate_usercode.py
@@ -0,0 +1,29 @@
+#bash script for many
+# for i in {1..1000}; do python3 simulate_usercode.py &; done
+
+
+from time import sleep
+import requests
+import pdb
+
+url = "http://127.0.0.1:5000/newtoken/"
+payload = {}
+headers= {}
+token_response = requests.request("POST", url, headers=headers, data = payload)
+token_text = token_response.json()['token']
+# pdb.set_trace()
+# token_text = response.text.encode('utf8')
+
+print(token_text)
+
+url = "http://127.0.0.1:5000/update/?token=%s&obs=OBSERVATION"%token_text
+
+payload = {}
+headers = {
+
+}
+i = 0
+while True:
+	i += 1.03
+	response = requests.request("POST", url.replace("OBSERVATION",str(i),1), headers=headers, data = payload)
+	sleep(0.5)
