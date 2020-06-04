405fd1a553892b2dc0dc93812a75de9b1519729c
bc <brian.cohn@usc.edu>
2020-06-04 10:20:59 -0700

    starting deploy attempts

---
+++ README.md
@@ -0,0 +1,6 @@
+# setup
+```bash
+
+sudo apt-get install python3-pip python3-dev nginx python3-venv -y
+python3 -m venv myenv 
+```
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd flaskapp
 #pip3 install -r requirements.txt
-sudo python3 app.py
+#sudo python3 app.py
+gunicorn --bind 0.0.0.0:5000 wsgi:app
