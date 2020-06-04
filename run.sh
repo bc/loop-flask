3f3108cc4509205286e3d06ff3b6ffb9a1efb8f9
bc <brian.cohn@usc.edu>
2020-06-04 12:14:21 -0700

    new gunicorn approach

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-cd flaskapp
-#pip3 install -r requirements.txt
+sudo cp src/app.service /etc/systemd/system/app.service
+pip3 install -r requirements.txt
+sudo systemctl status app
+sudo systemctl stop app
+sudo systemctl disable app
+sudo systemctl start app
+sudo systemctl reload app
+sudo systemctl enable app
 #sudo python3 app.py
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+#gunicorn --bind 0.0.0.0:5000 wsgi:app
