b792460f1632a4026999067a014fe759c4839e54
bc <brian.cohn@usc.edu>
2020-06-04 17:14:16 -0700

    op;

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 sudo cp app.service /etc/systemd/system/app.service
 pip3 install -r requirements.txt
-sudo systemctl status app
-sudo systemctl stop app
-sudo systemctl disable app
-sudo systemctl start app
-sudo systemctl reload app
-sudo systemctl enable app
+
 #sudo python3 app.py
-#gunicorn --bind 0.0.0.0:5000 wsgi:app
+#gunicorn --bind 0.0.0.0:5000 --debug-level=debug --access-logfile /home/deployuser/log.txt wsgi:app
