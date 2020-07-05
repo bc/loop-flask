2fdf7dbe37a631c3cb5be028e258923ac1b02211
Brian Cohn <bc@alumni.usc.edu>
2020-07-04 17:54:51 -0700

    new gist

--- src/static/webclient.js
+++ src/static/webclient.js
@@ -217,7 +217,7 @@ function get_contactinfo_from_server(status_icon_id) {
 }
 
 //Generate Code Snippets
-const client_cpu_link = "https://gist.githubusercontent.com/bc/766fb0c70e229b4018caf7c6cbc133e3/raw/05460d265a504159d443da7ef3f2e8efdb4ec235/client_cpu.py"
+const client_cpu_link = "https://gist.githubusercontent.com/bc/766fb0c70e229b4018caf7c6cbc133e3/raw/6b208f7782fa4fc5746db004735657613593c2c5/client_cpu.py"
 document.getElementById("bash_cpu_code_snippet").innerText = `curl -s ${client_cpu_link} >> /tmp/cpu_runner.py && python3 /tmp/cpu_runner.py ${window.location.protocol}//${window.location.host} ${get_token_from_param()}`
 document.getElementById("python_obs_code_snippet").innerText = `import requests;print(requests.request("POST","${window.location.protocol}//${window.location.host}/update_obs/?token=${token}&obs=%s"%val, headers={}, data = {}).text.encode('utf8'))`
 document.getElementById("curl_obs_code_snippet").innerText = `curl --location --request POST '${window.location.protocol}//${window.location.host}/update_obs/?token=${token}&obs=0.22'`
