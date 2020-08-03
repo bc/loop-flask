235a4e4bd64884ee8db9f7229474c77c9fbd30b2
bc <bc@alumni.usc.edu>
2020-08-02 17:59:17 -0700

    working xy plot with multiple axes

--- src/static/webclient.js
+++ src/static/webclient.js
@@ -226,4 +226,15 @@ document.getElementById("curl_obs_code_snippet").innerText = curl_command_obs
 document.getElementById("matlab_obs_code_snippet").innerText = `system("${curl_command_obs} &");`
 document.getElementById("python_obs_code_snippet").innerText = `import requests;print(requests.request("POST","${window.location.protocol}//${window.location.host}/update_obs/?token=${token}&obs=%s"%val, headers={}, data = {}).text.encode('utf8'))`
 
-var ctx = document.getElementById('myChart').getContext('2d');
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myLineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: [{
+//     x: 10,
+//     y: 20
+// }, {
+//     x: 15,
+//     y: 10
+// }],
+//     options:
+// });
